53111eacf0ff18d6aa0b5528366f05b6b1ac289f
Talwinder Singh <talwinder.s.nahal@gmail.com>
2024-01-07 18:18:03 +0000

    Add DOM elements to script.js.

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+let btnRef = document.querySelectorAll(".button-option");
+let popupRef = document.querySelectorAll(".popup");
+let newgameBtn = document.getElementById("new-game");
+let restartBtn = document.getElementById("restart");
+let msgRef = document.getElementById("message");
