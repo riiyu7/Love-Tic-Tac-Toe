21f6dd04971c23b9f8fed33d714a297ddb4f4656
Talwinder Singh <talwinder.s.nahal@gmail.com>
2024-01-07 19:27:41 +0000

    Add popup message for win function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
+// DOM Elements 
 let btnRef = document.querySelectorAll(".button-option");
-let popupRef = document.querySelectorAll(".popup");
+let popupRef = document.querySelector(".popup");
 let newgameBtn = document.getElementById("new-game");
 let restartBtn = document.getElementById("restart");
 let msgRef = document.getElementById("message");
+
 // Winning Pattern Array
-let WinningPattern = [
+let winningPattern = [
     [0, 1, 2],
     [0, 3, 6],
     [2, 5, 8],
@@ -19,48 +21,59 @@ let WinningPattern = [
 let xTurn = true;
 let count = 0;
 
+//Disable All Buttons
+const disableButtons = () => {
+    btnRef.forEach((element) => (element.disabled = true));
+    //enable popup
+    popupRef.classList.remove("hide");
+}
+
 //This Function is executed when a player wins
-const Winfunction = (letter) => {
+const winfunction = (letter) => {
     diasbleButtons();
 };
 
 //Win Logic
-const winchecker = () => {
-        //Loop through all win patterns
-        for (let i of WinningPattern) {
-            let [element1, element2, element3] = [
-                btnRef[i[0]].innerText,
-                btnRef[i[1]].innerText,
-                btnRef[i[2]].innerText,
-            ];
-            //Check if elements are filled
-            if(element1 != "" && (element2 != "") & (element3 !="")){
-                if (element1 == element2 && element2 == element3){
-                    //If all 3 buttons have the same values then pass the value to winFunction
-                    winFunction(element1);
-                }
-            }
+const winChecker = () => {
+    //Loop through all win patterns
+    for (let i of winningPattern) {
+      let [element1, element2, element3] = [
+        btnRef[i[0]].innerText,
+        btnRef[i[1]].innerText,
+        btnRef[i[2]].innerText,
+      ];
+
+      //Check if elements are filled
+
+      //If 3 empty elements are same and would give win as would
+      if (element1 != "" && (element2 != "") & (element3 != "")) {
+        if (element1 == element2 && element2 == element3) {
+          //If all 3 buttons have same values then pass the value to winFunction
+          winfunction(element1);
         }
+      }
+    }
+  };
         //Display X/O on click
         btnRef.forEach((element) => {
             element.addEventListener("click", () => {
-                if (xTurn) {
-                    xTurn = false;
-                    //Display X
-                    element.innerText = "X";
-                    element.disabled = true;
-                } else {
-                    xTurn = true;
-                    //Display Y
-                    element.innerText = "O";
-                    element.disabled = true;
-                }
-                // Increment count on each click
-                count += 1;
-                if (count === 9) {
-                    //Its a draw since there is only a 9 boxes on the grid
-                }
-                //check for win on every click
-                winchecker();
+              if (xTurn) {
+                xTurn = false;
+                //Display X
+                element.innerText = "X";
+                element.disabled = true;
+              } else {
+                xTurn = true;
+                //Display Y
+                element.innerText = "O";
+                element.disabled = true;
+              }
+              //Increment count on each click
+              count += 1;
+              if (count == 9) {
+                drawFunction();
+              }
+              //Check for win on every click
+              winChecker();
             });
-        });
+          });
