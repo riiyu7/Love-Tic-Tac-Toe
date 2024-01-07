2758a0a8d9de8eb9788a05288f587a657f4c1d4f
Talwinder Singh <talwinder.s.nahal@gmail.com>
2024-01-07 18:22:35 +0000

    Add winning pattern array to script.js.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,14 @@ let popupRef = document.querySelectorAll(".popup");
 let newgameBtn = document.getElementById("new-game");
 let restartBtn = document.getElementById("restart");
 let msgRef = document.getElementById("message");
+// Winning Pattern Array
+let WinningPattern = [ 
+    [0,1,2],
+    [0,3,6],
+    [2,5,8],
+    [6,7,8],
+    [3,4,5],
+    [1,4,7],
+    [0,4,8],
+    [2,4,6],
+];
